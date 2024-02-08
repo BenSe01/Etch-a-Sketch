292516e506631116266f8fc10a1f770ce443d383
BenSe01 <150089923+BenSe01@users.noreply.github.com>
2024-02-08 14:14:57 +0100

    Added functionality to Reset and Grid Size Button

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-let rows = 16;
-let cols = 16;
+let squaresPerSide = 16
 const gridSide = 600;
 
 const gridContainer = document.querySelector(".grid-container");
-gridContainer.style.width = `${gridSide}px`;
-gridContainer.style.height = `${gridSide}px`;
+gridContainer.style.width = gridContainer.style.height = `${gridSide}px`;
 
 
-function createGrid() {
-    for (let i = 0; i < (rows * cols); i++) {
+function createGrid(squaresPerSide) {
+    const numOfSquares = (squaresPerSide * squaresPerSide);
+    const widthOrHeight = `${gridSide / squaresPerSide - 2}px`
+
+
+    for (let i = 0; i < numOfSquares; i++) {
        const gridCell = document.createElement("div");
-       
-       gridCell.style.width = `${(gridSide / cols) - 2}px`;
-       gridCell.style.height = `${(gridSide / rows) - 2}px`;
+       gridCell.style.width = gridCell.style.height = widthOrHeight;
        gridCell.classList.add("cell"); 
 
        gridContainer.appendChild(gridCell);
@@ -36,9 +36,30 @@ function createGrid() {
     
 }
 
+const sizeBTN = document.querySelector("#sizeBTN");
 
 
-createGrid();
+function getSize() {
+    let newSize = prompt("How many cells per Side do you want? (2 - 100)");
+    
+    if (newSize > 100 || newSize < 1) {
+        newSize = prompt("Invalid Input, please enter a Number from 2 to 100");
+    }else if (newSize >= 2 && newSize <= 100) {
+        removeGrid();
+        createGrid(newSize);
+    }else if (newSize.typeOf !== "number") {
+        newSize = prompt("Invalid Input, please enter a Number from 2 to 100");
+    }
+}
+
+function removeGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+
+createGrid(16);
 
 
 
